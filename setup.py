973c9e5c34c938b733946829f479242dcf540adf
Praveenku32k <68581081+Praveenku32k@users.noreply.github.com>
2021-09-23 21:47:17 +0530

    compete new update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "Praveenku32k"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author=USER_NAME,
     author_email="praveenku32k@gmail.com",
     description="its an implimentation of Perceptron",
